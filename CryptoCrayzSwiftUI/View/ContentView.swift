7f0ef443b38fcce9f94b082c7de8e8029ca2c2ae
FurkanMDemiray <furkanmdemiray@gmail.com>
2023-12-31 16:47:52 +0300

    added view funcs

--- CryptoCrayzSwiftUI/View/ContentView.swift
+++ CryptoCrayzSwiftUI/View/ContentView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
 
-    var cryptoList = [Crypto]()
+    @ObservedObject var cryptoListViewModel = CryptoListViewModel()
+
+    init() {
+        self.cryptoListViewModel = CryptoListViewModel()
+    }
 
 
     var body: some View {
-        List(cryptoList) { crypto in
-            Text(crypto.currency)
+        NavigationView {
+
+            List(cryptoListViewModel.cryptos, id: \.id) { crypto in
+                VStack {
+                    Text(crypto.currency)
+                        .font(.title3)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text(crypto.price)
+                        .font(.subheadline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(Color.black)
+                }.padding(2)
+                    
+            }.navigationTitle("CryptoCrayz")
+
+        }.onAppear() {
+            self.cryptoListViewModel.downloadCryptos(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
         }
     }
 }
